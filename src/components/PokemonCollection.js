97ab6d2e84760642030435230c38b34ed6f67e68
Kristina Peru <krissy.germanov@gmail.com>
2022-03-04 08:17:35 -0700

    Display cards && toggle between front and back

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,10 +2,19 @@ import React from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection() {
+function PokemonCollection({pokeData}) {
+  const pokeCollection = pokeData.map((pokeObj) => (
+    <PokemonCard 
+      key={pokeObj.id}
+      name={pokeObj.name}
+      hp={pokeObj.hp}
+      sprites={pokeObj.sprites}
+      pokeData={pokeObj}
+    />
+  ))
   return (
     <Card.Group itemsPerRow={6}>
-      <h1>Hello From Pokemon Collection</h1>
+      <h1>{pokeCollection}</h1>
     </Card.Group>
   );
 }
