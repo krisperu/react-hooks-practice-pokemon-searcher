f00442a665edbe786c366ed18510a323d529c43b
Kristina Peru <krissy.germanov@gmail.com>
2022-03-04 07:39:14 -0700

    Fetch

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+  const baseUrl = "http://localhost:3001"
+  const [pokeData, setPokeData] = useState([])
+
+
+  useEffect(() => {
+    fetch(baseUrl + "/Pokemon")
+    .then(res => res.json())
+    .then(setPokeData)
+  }, [])
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
@@ -13,7 +23,7 @@ function PokemonPage() {
       <br />
       <Search />
       <br />
-      <PokemonCollection />
+      <PokemonCollection pokeData={pokeData} />
     </Container>
   );
 }
